d686f5029fbdb3ef5c2c1cd4b9bb78606a0e18fb
Mykolas Raudžius <s063337@ad.viko.lt>
2026-02-26 13:46:14 +0200

    feat: add main program menu action text

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    bool programIsRunning = true;
+    while (programIsRunning)
+    {
+        cout << "Pasirinkite norima veiksma, ivesdami jo numeri:" << endl;
+        cout << "Pridėti mokinį - 1" << "\n";
+        cout << "Perziureti mokiniu pazymius - 2" << "\n";
+        cout << "Atnaujinti mokiniu pazymius - 3" << "\n";
+        cout << "Pasalinti mokini is saraso - 4" << "\n";
+        cout << "Iseiti - 0" << "\n";
+
+        switch ()
+        {
+            case 1:
+
+        }
+    }
+    return 0;
+}
